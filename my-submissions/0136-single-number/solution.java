ffd0e4999b3270e2b1ba6ebbcfa5306b0f4567ad
SAKSHAM KHANDELWAL <sakshamkhandelwal19207@gmail.com>
2025-12-10 19:29:30 +0000

    Sync LeetCode submission Runtime - 9 ms (29.16%), Memory - 47.3 MB (6.12%)

---
+++ my-submissions/0136-single-number/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i+=2){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        Solution s=new Solution();
+        int n=sc.nextInt();
+        int[] nums=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        int k=s.singleNumber(nums);
+        System.out.print(k);
+    }
+}
