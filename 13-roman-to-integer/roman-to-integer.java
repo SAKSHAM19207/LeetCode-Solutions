5ee71416b6a0a598c1a8478482dcd413adf39f29
SAKSHAM KHANDELWAL <sakshamkhandelwal19207@gmail.com>
2025-12-20 00:00:30 +0530

    Time: 2 ms (99.95%) | Memory: 46.4 MB (57.69%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -2,11 +2,9 @@ class Solution {
     public int romanToInt(String s) {
         int total = 0;
         int prev = 0;
-
         for (int i = s.length() - 1; i >= 0; i--) {
             int curr = 0;
             char c = s.charAt(i);
-
             if (c == 'I') curr = 1;
             else if (c == 'V') curr = 5;
             else if (c == 'X') curr = 10;
@@ -14,7 +12,6 @@ class Solution {
             else if (c == 'C') curr = 100;
             else if (c == 'D') curr = 500;
             else if (c == 'M') curr = 1000;
-
             if (curr < prev) {
                 total -= curr;
             } else {
